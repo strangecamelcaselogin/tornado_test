12d739ccccf6ba07513956b39a51b82f6847feb7
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-07-30 21:43:24 +0300

    main: explicits params for server

--- main.py
+++ main.py
@@ -15,24 +15,26 @@ def setup():
     # tornado.autoreload.start()  # Включение не дает запустить много процессов
 
 
-def make_app():
-    return tornado.web.Application([
-        (r'/slow', HellowWorldHandler),
-        (r'/fast', FastHandler)
-    ])
+def make_app(rout_rules):
+    return tornado.web.Application(rout_rules)
 
 
 if __name__ == '__main__':
+    port = 8888
+    processes = 1
+    routs = [(r'/slow', HellowWorldHandler),
+             (r'/fast', FastHandler)]
+
     gen_log.info('prepare to start server...')
     setup()
     gen_log.info('setup ends...')
 
-    app = make_app()
+    app = make_app(routs)
 
     try:
         server = HTTPServer(app)
-        server.bind(8888)
-        server.start(1)
+        server.bind(port)
+        server.start(processes)
         gen_log.info('start server...')
 
         IOLoop.current().start()
