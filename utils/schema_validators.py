4d47536cf2c77f02989b70be3678d355c4042e22
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-07-30 21:32:51 +0300

    req validators: add docstrings

--- utils/schema_validators.py
+++ utils/schema_validators.py
@@ -4,6 +4,12 @@ from voluptuous import Schema, MultipleInvalid, PREVENT_EXTRA
 
 
 def construct_response(exception):
+    """
+    :param exception: MultipleInvalid exception
+    :return: json
+
+    Делает json с ошибками из MultipleInvalid exception.
+    """
     result = []
     if isinstance(exception, MultipleInvalid):
         for e in exception.errors:
@@ -16,6 +22,21 @@ def construct_response(exception):
 
 
 def dvalidate(schema, required=False, extra=PREVENT_EXTRA):
+    """
+    :param schema: dict в формате voluptuous
+    :param required: если True, все параметры схемы оязательны
+    :param extra: стратегия поведения в случае нехватки или избытка параметров
+    :return: результат функции если параметры валидны, иначе сообщение об ошибке
+
+    декоратор для валидации параметров у tornado хендлеров
+
+    использование:
+        class SomeTornadoHandler:
+            @dvalidate({'a': int, 'b': str})
+            def get(self):
+                self.write('success')
+
+    """
     assert isinstance(schema, dict)
     schema = Schema(schema, required, extra)
 
@@ -36,5 +57,10 @@ def dvalidate(schema, required=False, extra=PREVENT_EXTRA):
 
 
 def mvalidate(required=False, extra=PREVENT_EXTRA, **schema):
+    """
+    параметры как у dvalidate
+
+    ручной вариант dvalidate
+    """
     schema = Schema(schema, required, extra)
     return schema
